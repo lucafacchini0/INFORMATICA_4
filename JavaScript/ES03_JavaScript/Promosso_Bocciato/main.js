eb7662789328c585351a18c813c1380b9077100a
lucafacchini0 <luca.facchini1@protonmail.com>
2024-11-24 22:03:29 +0000

    fixed wrong function call

--- JavaScript/ES03_JavaScript/Promosso_Bocciato/main.js
+++ JavaScript/ES03_JavaScript/Promosso_Bocciato/main.js
@@ -1,2 +1,8 @@
 let voto = 8;
+console.log("Il voto è: " + voto);
 let esito = (voto >= 6) ? "Promosso" : "Bocciato";
+console.log(esito);
+voto = 3;
+console.log("Il voto è: " + voto);
+esito = (voto >= 6) ? "Promosso" : "Bocciato";
+console.log(esito);
